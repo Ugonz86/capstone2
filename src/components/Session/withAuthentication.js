9c12de5672af0f85468cc80e6032b654d5742557
Ugonz86 <ugonzalez86@gmail.com>
2020-03-05 15:34:50 -0800

    Add updated routes. Now routes are protected by withAuthorization

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
-const withAuthentication = Component => {
-  class WithAuthentication extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        authUser: null,
-      };
-    }
+import * as ROUTES from '../../constants/routes';
+
+const withAuthorization = condition => Component => {
+  class WithAuthorization extends React.Component {
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
-        authUser => {
-          authUser
-            ? this.setState({ authUser })
-            : this.setState({ authUser: null });
-        },
-      );
+      this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+        if (!condition(authUser)) {
+          this.props.history.push(ROUTES.SIGN_IN);
+        }
+      });
     }
     componentWillUnmount() {
       this.listener();
     }
     render() {
       return (
-        <AuthUserContext.Provider value={this.state.authUser}>
-          <Component {...this.props} />
-        </AuthUserContext.Provider>
+        <AuthUserContext.Consumer>
+          {authUser =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
       );
     }
   }
-  return withFirebase(WithAuthentication);
+  return compose(
+    withRouter,
+    withFirebase,
+  )(WithAuthorization);
 };
-export default withAuthentication;
+
+export default withAuthorization;
