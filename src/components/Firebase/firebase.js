d74ec06f7e9d379a829b596e744d0883cf7114e5
Ugonz86 <ugonzalez86@gmail.com>
2020-03-05 15:36:46 -0800

    Set up database in react

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,6 +15,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
   // *** Auth API ***
   doCreateUserWithEmailAndPassword = (email, password) =>
@@ -27,5 +29,10 @@ class Firebase {
     doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
     doPasswordUpdate = password =>
         this.auth.currentUser.updatePassword(password);
+
+    // *** User API ***
+    user = uid => this.db.ref(`users/${uid}`);
+    users = () => this.db.ref('users');
 }
+
 export default Firebase;
