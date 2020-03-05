309a3ddb690c9cf98107f558463ff7186a52a198
Ugonz86 <ugonzalez86@gmail.com>
2020-03-05 15:44:55 -0800

    make possible to create and retrieve users from your realtime database

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -27,9 +27,19 @@ const INITIAL_STATE = {
   }
   onSubmit = event => {
     const { username, email, passwordOne } = this.state;
+
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
+        // Create a user in your Firebase realtime database
+        return this.props.firebase
+          .user(authUser.user.uid)
+          .set({
+            username,
+            email,
+          });
+      })
+      .then(() => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
       })
@@ -37,7 +47,7 @@ const INITIAL_STATE = {
         this.setState({ error });
       });
     event.preventDefault();
-  }
+  };
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
