0eed612686ac234ea3d2fff59f66980f616824c2
Ugonz86 <ugonzalez86@gmail.com>
2020-03-12 12:00:27 -0700

    update payments page

--- src/components/Payments/index.js
+++ src/components/Payments/index.js
@@ -14,23 +14,23 @@ function Payments () {
       marginBottom: '20px',
       padding: '10px',
       color: '#edeff5'}}>
-    <h1 style={{color: '#333'}}>Payment overview</h1>
+    <h1 style={{color: '#333'}}>Payment Overview</h1>
     </div>
 
       <div style={{backgroundColor: '#4265ff', padding: '10px', position: 'relative', borderRadius: '10px', height: '100px', margin: 'auto', width: '50%', marginBottom: '20px' }}>
-            <h2><Link style={{color: 'white'}} to={ROUTES.PAYMENTFORM}>Make a payment here</Link></h2>
+            <h2><Link style={{color: 'white'}} to={ROUTES.PAYMENTFORM}>Make a Payment</Link></h2>
         </div>
 
       <div style={{backgroundColor: '#4265ff', padding: '10px', position: 'relative', color: 'white', borderRadius: '10px', height: '100px', margin: 'auto', width: '50%', marginBottom: '20px' }}>
-          <h2>Upcoming payments here</h2>
+          <h2>Upcoming Payments</h2>
       </div>
 
       <div style={{backgroundColor: '#4265ff', padding: '10px', position: 'relative', color: 'white', borderRadius: '10px', height: '100px', margin: 'auto', width: '50%', marginBottom: '20px' }}>
-          <h2>Payment history here</h2>
+          <h2>Payment History</h2>
       </div>
 
       <div style={{backgroundColor: '#4265ff', padding: '10px', position: 'relative', color: 'white', borderRadius: '10px', height: '100px', margin: 'auto', width: '50%' }}>
-          <h2>Set Auto Payment here</h2>
+          <h2>Set Auto Payments</h2>
       </div>
   </div>
    );
