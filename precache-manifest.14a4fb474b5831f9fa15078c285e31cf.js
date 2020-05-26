e312d816f3379257f73863da5575861b3bc84927
Ugonz86 <ugonzalez86@gmail.com>
2020-05-26 16:27:07 -0700

    Updates

---
+++ precache-manifest.14a4fb474b5831f9fa15078c285e31cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88f4bbf77968aa1b20abf5a94162bbd",
+    "url": "/eBills/index.html"
+  },
+  {
+    "revision": "4a9cde703cf0491e2562",
+    "url": "/eBills/static/css/main.25345dd1.chunk.css"
+  },
+  {
+    "revision": "4a2aa5a2cc7187aef41f",
+    "url": "/eBills/static/js/2.ab159214.chunk.js"
+  },
+  {
+    "revision": "494f8b7a8f4173d4158e06f0b7be3830",
+    "url": "/eBills/static/js/2.ab159214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9cde703cf0491e2562",
+    "url": "/eBills/static/js/main.bca0cc07.chunk.js"
+  },
+  {
+    "revision": "78947112a969fc16d592",
+    "url": "/eBills/static/js/runtime-main.442381a2.js"
+  }
+]);
